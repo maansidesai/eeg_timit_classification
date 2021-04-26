5b0db44e6b8ceeb9b125dbf6316637dc9ddd6bce
Garret Kurteff <grt.krtf@gmail.com>
2021-04-26 17:14:19 -0500

    meeting with maansi 4/26, next step alex office hours

--- tools.py
+++ tools.py
@@ -6,6 +6,7 @@ import numpy as np
 from numpy.polynomial.polynomial import polyfit
 # from audio_tools import spectools, fbtools, phn_tools #use custom functions for linguistic/acoustic alignment
 from scipy.io import wavfile
+from scipy import signal
 from scipy.spatial import ConvexHull, convex_hull_plot_2d
 import os
 import re
@@ -100,74 +101,72 @@ def loadEEGh5(subject, stimulus_class, data_dir,
 			print(wav_name)
 			stim_dict[wav_name] = []
 			resp_dict[wav_name] = []
-			try:
-				epochs_data = fh['/%s/%s/resp/%s/epochs' %(stimulus_class, wav_name, subject)][:]
-				phnfeatmat = fh['/%s/%s/stim/phn_feat_timings' %(stimulus_class, wav_name)][:]
-				ntimes = phnfeatmat.shape[1] #always resample to the size of phnfeat 
-				if binarymat:
-					phnmat = fh['/%s/%s/stim/phn_timings' %(stimulus_class, wav_name)][:] 
-					stim_dict[wav_name].append(phnmat)
-					ntimes = phnmat.shape[1]
-					print('phnmat shape is:')
-					print(phnmat.shape)
-				if binaryfeatmat:
-					stim_dict[wav_name].append(phnfeatmat)
-					print('phnfeatmat shape is:')
-					print(phnfeatmat.shape)
-				if envelope:
-					envs = fh['/%s/%s/stim/envelope' %(stimulus_class, wav_name)][:] 
-					envs = scipy.signal.resample(envs, ntimes) #resampling to size of phnfeat
-					stim_dict[wav_name].append(envs.T)
-					print('envs shape is:')
-					print(envs.shape)
-				if pitch:
-					pitch_mat = fh['/%s/%s/stim/pitches' %(stimulus_class, wav_name)][:] 
-					pitch_mat = scipy.signal.resample(pitch_mat, ntimes) #resample to size of phnfeat
-					pitch_mat = np.atleast_2d(pitch_mat)
-					stim_dict[wav_name].append(pitch_mat)
-					print('pitch_mat shape is:')
-					print(pitch_mat.shape)	
-				if binned_pitches:
-					binned_p = fh['/%s/%s/stim/binned_pitches' %(stimulus_class, wav_name)][:] 
-					#binned_p = scipy.signal.resample(binned_p, ntimes) #resample to size of phnfeat
-					binned_p = np.atleast_2d(binned_p)
-					stim_dict[wav_name].append(binned_p.T)
-					print('binned pitch shape is:')
-					print(binned_p.shape)				
-				if gabor_pc10:
-					gabor_pc10_mat = fh['/%s/%s/stim/gabor_pc10' %(stimulus_class, wav_name)][:]
-					stim_dict[wav_name].append(gabor_pc10_mat.T)
-					print('gabor_mat shape is:')
-					print(gabor_pc10_mat.shape)  
-				if spectrogram:
-					specs = fh['/%s/%s/stim/spec' %(stimulus_class, wav_name)][:] 
-					specs = scipy.signal.resample(specs, ntimes, axis=1)
-					new_freq = 15 #create new feature size, from 80 to 15. Easier to fit STRF with the specified time delay
-					specs = scipy.signal.resample(specs, new_freq, axis=0)
-					stim_dict[wav_name].append(specs)
-					print('specs shape is:')
-					print(specs.shape)
-					freqs = fh['/%s/%s/stim/freqs' %(stimulus_class, wav_name)][:]
-				if spectrogram_scaled:
-					specs = fh['/%s/%s/stim/spec' %(stimulus_class, wav_name)][:] 
-					specs = scipy.signal.resample(specs, ntimes, axis=1)
-					new_freq = 15 #create new feature size, from 80 to 15. Easier to fit STRF with the specified time delay
-					specs = scipy.signal.resample(specs, new_freq, axis=0)
-					specs  = specs/np.abs(specs).max()
-					stim_dict[wav_name].append(specs)
-					print('specs shape is:')
-					print(specs.shape)
-				if scene_cut:
-					s_cuts = fh['/%s/%s/stim/scene_cut' %(stimulus_class, wav_name)][:] 
-					s_cuts = scipy.signal.resample(s_cuts, ntimes, axis=1)
-					stim_dict[wav_name].append(s_cuts)
-					print('scene cut shape is:')
-					print(s_cuts.shape)
-			
-					#return freqs once
-					freqs = fh['/%s/%s/stim/freqs' %(stimulus_class, wav_name)][:]
-			except Exception:
-				traceback.print_exc()
+			epochs_data = fh['/%s/%s/resp/%s/epochs' %(stimulus_class, wav_name, subject)][:]
+			phnfeatmat = fh['/%s/%s/stim/phn_feat_timings' %(stimulus_class, wav_name)][:]
+			ntimes = phnfeatmat.shape[1] #always resample to the size of phnfeat 
+			if binarymat:
+				phnmat = fh['/%s/%s/stim/phn_timings' %(stimulus_class, wav_name)][:] 
+				stim_dict[wav_name].append(phnmat)
+				ntimes = phnmat.shape[1]
+				print('phnmat shape is:')
+				print(phnmat.shape)
+			if binaryfeatmat:
+				stim_dict[wav_name].append(phnfeatmat)
+				print('phnfeatmat shape is:')
+				print(phnfeatmat.shape)
+			if envelope:
+				envs = fh['/%s/%s/stim/envelope' %(stimulus_class, wav_name)][:] 
+				envs = scipy.signal.resample(envs, ntimes) #resampling to size of phnfeat
+				stim_dict[wav_name].append(envs.T)
+				print('envs shape is:')
+				print(envs.shape)
+			if pitch:
+				pitch_mat = fh['/%s/%s/stim/pitches' %(stimulus_class, wav_name)][:] 
+				pitch_mat = scipy.signal.resample(pitch_mat, ntimes) #resample to size of phnfeat
+				pitch_mat = np.atleast_2d(pitch_mat)
+				stim_dict[wav_name].append(pitch_mat)
+				print('pitch_mat shape is:')
+				print(pitch_mat.shape)	
+			if binned_pitches:
+				binned_p = fh['/%s/%s/stim/binned_pitches' %(stimulus_class, wav_name)][:] 
+				#binned_p = scipy.signal.resample(binned_p, ntimes) #resample to size of phnfeat
+				binned_p = np.atleast_2d(binned_p)
+				stim_dict[wav_name].append(binned_p.T)
+				print('binned pitch shape is:')
+				print(binned_p.shape)				
+			if gabor_pc10:
+				gabor_pc10_mat = fh['/%s/%s/stim/gabor_pc10' %(stimulus_class, wav_name)][:]
+				stim_dict[wav_name].append(gabor_pc10_mat.T)
+				print('gabor_mat shape is:')
+				print(gabor_pc10_mat.shape)  
+			if spectrogram:
+				specs = fh['/%s/%s/stim/spec' %(stimulus_class, wav_name)][:] 
+				specs = scipy.signal.resample(specs, ntimes, axis=1)
+				new_freq = 15 #create new feature size, from 80 to 15. Easier to fit STRF with the specified time delay
+				specs = scipy.signal.resample(specs, new_freq, axis=0)
+				stim_dict[wav_name].append(specs)
+				print('specs shape is:')
+				print(specs.shape)
+				freqs = fh['/%s/%s/stim/freqs' %(stimulus_class, wav_name)][:]
+			if spectrogram_scaled:
+				specs = fh['/%s/%s/stim/spec' %(stimulus_class, wav_name)][:] 
+				specs = scipy.signal.resample(specs, ntimes, axis=1)
+				new_freq = 15 #create new feature size, from 80 to 15. Easier to fit STRF with the specified time delay
+				specs = scipy.signal.resample(specs, new_freq, axis=0)
+				specs  = specs/np.abs(specs).max()
+				stim_dict[wav_name].append(specs)
+				print('specs shape is:')
+				print(specs.shape)
+			if scene_cut:
+				s_cuts = fh['/%s/%s/stim/scene_cut' %(stimulus_class, wav_name)][:] 
+				s_cuts = scipy.signal.resample(s_cuts, ntimes, axis=1)
+				stim_dict[wav_name].append(s_cuts)
+				print('scene cut shape is:')
+				print(s_cuts.shape)
+		
+				#return freqs once
+				freqs = fh['/%s/%s/stim/freqs' %(stimulus_class, wav_name)][:]
+
 				
 			if eeg_epochs:
 				try: 
